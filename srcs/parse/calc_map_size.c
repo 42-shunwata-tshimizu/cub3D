36a2141ed8f55d2699c20c45888ccb5906a39e31
tshimizu <f2021212010@gmail.com>
2026-04-25 18:40:19 +0900

    fix:remove-unused-val

--- srcs/parse/calc_map_size.c
+++ srcs/parse/calc_map_size.c
@@ -6,7 +6,7 @@
 /*   By: tshimizu <tshimizu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 17:27:20 by tshimizu          #+#    #+#             */
-/*   Updated: 2026/04/25 18:38:40 by tshimizu         ###   ########.fr       */
+/*   Updated: 2026/04/25 18:39:54 by tshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	calc_map_size(t_game *game)
 {
 	int		width;
 	int		height;
-	char	*current_line;
 	int		len;
 
 	height = 0;
